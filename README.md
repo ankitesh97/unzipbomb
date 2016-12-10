70ea36b1fb271f06e1793b0d76e26c6fa4313252
Ankitesh Gupta <ankiteshguptas@gmail.com>
2016-12-10 13:45:51 +0530

    edited some files

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # unzipbomb
-A python module which defuses the zipbomb also can extarct all the zip files that are contained in a specific root folder
+A python module which defuses the zipbomb also can extract all the zip files that are contained in a specific root folder
 
 please suggest if any changes are required
 
 Read the usage.pdf for the usage
+
+note: This version is only for unix system, will make it compatible for windows in further releases
--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ setup(
   name = 'unzipbomb',
   packages = ['unzipbomb'],
   version = '1.1',
-  description = 'A python module which defuses the zipbomb also can extarct all the zip files that are contained in a specific root folder, please read usage.pdf from github repo',
+  description = 'A python module which defuses the zipbomb also can extract all the zip files that are contained in a specific root folder, please read usage.pdf from github repo',
   author = 'Ankitesh Gupta, Saif Vazir',
   author_email = 'ankiteshguptas@gmail.com,ssv.saif@gmail.com',
   url = 'https://github.com/ankitesh97/unzipbomb',
   download_url = 'https://github.com/ankitesh97/unzipbomb/tarball/v1.1',
-  keywords = ['unzipbomb', 'zipbomb', 'extract', 'multiple zip files', 'zip bomb','unzip','unzip','zip','compress','decompress','extacrt multiple zip files'],
+  keywords = ['unzipbomb', 'zipbomb', 'extract', 'multiple zip files', 'zip bomb','unzip','unzip','zip','compress','decompress','extract multiple zip files'],
   classifiers = [],
 )
