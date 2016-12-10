da05d7acd55144d4dab965ede28e0e26fc46ec54
Ankitesh Gupta <ankiteshguptas@gmail.com>
2016-12-10 12:59:07 +0530

    edited setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'unzipbomb',
   packages = ['unzipbomb'],
-  version = '1.0',
+  version = '1.1',
   description = 'A python module which defuses the zipbomb also can extarct all the zip files that are contained in a specific root folder',
   author = 'Ankitesh Gupta, Saif Vazir',
   author_email = 'ankiteshguptas@gmail.com,sv.saif@gmail.com',
   url = 'https://github.com/ankitesh97/unzipbomb',
-  download_url = 'https://github.com/ankitesh97/unzipbomb/tarball/v1.0',
+  download_url = 'https://github.com/ankitesh97/unzipbomb/tarball/v1.1',
   keywords = ['unzipbomb', 'zipbomb', 'extract', 'multiple zip files', 'zip bomb','unzip','unzip','zip','compress','decompress','extacrt multiple zip files'],
   classifiers = [],
 )
